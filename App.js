8d0f164508c67b6b7dc92527caf01cc3febd3da5
Eda Belge <edabelge00@gmail.com>
2023-08-19 18:25:39 +0300

    Bottom tabs style added.

--- App.js
+++ App.js
@@ -5,15 +5,22 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from "./constants/styles";
+import { Ionicons } from "@expo/vector-icons"
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} />
-      <BottomTabs.Screen name='AllExpenses' component={AllExpenses} />
+    <BottomTabs.Navigator screenOptions={{
+      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      headerTintColor: "white",
+      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
+    }}>
+      <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} options={{ title: "Recent Expenses", tabBarLabel: "Recent", tabBarIcon: ({ color, size }) => (<Ionicons name="hourglass" size={size} color={color} />) }} />
+      <BottomTabs.Screen name='AllExpenses' component={AllExpenses} options={{ title: "All Expenses", tabBarLabel: "All Expenses", tabBarIcon: ({ color, size }) => (<Ionicons name="calendar" size={size} color={color} />) }} />
     </BottomTabs.Navigator>
   );
 }
@@ -24,7 +31,7 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{ headerShown: false }} />
           <Stack.Screen name='ManageExpenses' component={ManageExpense} />
         </Stack.Navigator>
       </NavigationContainer>
