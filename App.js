c8224936c425fb16305dddf4f3ae81a47ffb8d34
Eda Belge <edabelge00@gmail.com>
2023-08-24 15:48:55 +0300

    Recent expenses page's name changed

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ function ExpensesOverview() {
       tabBarActiveTintColor: GlobalStyles.colors.accent500,
       headerRight: ({ tintColor }) => <IconButton icon="add" size={24} color={tintColor} onPress={() => { navigation.navigate('ManageExpense') }} />
     })}>
-      <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} options={{ title: "Recent Expenses", tabBarLabel: "Recent", tabBarIcon: ({ color, size }) => (<Ionicons name="hourglass" size={size} color={color} />) }} />
+      <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} options={{ title: "Recent Expenses", tabBarLabel: "Recent Expenses", tabBarIcon: ({ color, size }) => (<Ionicons name="hourglass" size={size} color={color} />) }} />
       <BottomTabs.Screen name='AllExpenses' component={AllExpenses} options={{ title: "All Expenses", tabBarLabel: "All Expenses", tabBarIcon: ({ color, size }) => (<Ionicons name="calendar" size={size} color={color} />) }} />
     </BottomTabs.Navigator>
   );
