d4fa3406de4d0c8ec9db95d2ff73c866f5251d26
Eda Belge <edabelge00@gmail.com>
2023-08-24 14:32:51 +0300

    User input handling as generic added

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,10 +1,22 @@
 import { View, StyleSheet, Text } from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpenseForm() {
 
-    function amountChangedHandler() {
+    const [inputs, setInputs] = useState({
+        amount: "",
+        date: "",
+        description: ""
+    });
 
+    function inputChangedHandler(inputIdentifier, enteredValue) {
+        setInputs((currentInputs) => {
+            return {
+                ...currentInputs,
+                [inputIdentifier]: enteredValue
+            };
+        });
     }
 
     return <View style={styles.form}>
@@ -12,16 +24,20 @@ function ExpenseForm() {
         <View style={styles.inputsRow}>
             <Input style={styles.rowInput} label="Amount" textInputConfig={{
                 keyboardType: "decimal-pad",
-                onChangeText: amountChangedHandler,
+                value: inputs.amount,
+                onChangeText: inputChangedHandler.bind(this, "amount"),
             }} />
             <Input style={styles.rowInput} label="Date" textInputConfig={{
                 placeholder: "YYYY-MM-DD",
                 maxLength: 10,
-                onChangeText: () => { }
+                value: inputs.date,
+                onChangeText: inputChangedHandler.bind(this, "date"),
             }} />
         </View>
         <Input label="Description" textInputConfig={{
             multiline: true,
+            value: inputs.description,
+            onChangeText: inputChangedHandler.bind(this, "description"),
         }} />
     </View>
 }
